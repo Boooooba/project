8cf078923dae46eb15ed0e6422c0aacb690d900c
shota876 <sh.guliashvili1@gmail.com>
2023-09-19 12:14:27 -0700

    project header

---
+++ app.js
@@ -0,0 +1,31 @@
+const imgEl = document.querySelector('.img')
+const svgEl = document.querySelector('.svg')
+const hiddenEls = document.querySelectorAll('.hidden')
+const welcomingDiv = document.getElementById('welcomingText')
+const welcomingEngDiv = document.getElementById('welcomingTextEng')
+const offCanvas = document.getElementById('offcanvasDarkNavbar')
+const navBar = document.getElementById('navBar')
+
+  //images transition onload
+window.onload = () => {
+    imgEl.classList.add('show')
+    svgEl.classList.add('show')
+}
+
+
+  //texts transition on scroll
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting){
+            entry.target.classList.add('show')
+        }else{
+            entry.target.classList.remove('show')
+        }
+    })
+})
+
+
+hiddenEls.forEach((el) => observer.observe(el))
+
+
+
