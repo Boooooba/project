04d0123da5c429c86ca3fb2b51c8ec05fbce3925
dichi52 <123590330+dichi52@users.noreply.github.com>
2023-09-20 15:06:32 +0400

    code

--- app.js
+++ app.js
@@ -1,31 +1,41 @@
-const imgEl = document.querySelector('.img')
-const svgEl = document.querySelector('.svg')
-const hiddenEls = document.querySelectorAll('.hidden')
-const welcomingDiv = document.getElementById('welcomingText')
-const welcomingEngDiv = document.getElementById('welcomingTextEng')
-const offCanvas = document.getElementById('offcanvasDarkNavbar')
-const navBar = document.getElementById('navBar')
+const imgEl = document.querySelector(".img");
+const svgEl = document.querySelector(".svg");
+const hiddenEls = document.querySelectorAll(".hidden");
+const welcomingDiv = document.getElementById("welcomingText");
+const welcomingEngDiv = document.getElementById("welcomingTextEng");
+const offCanvas = document.getElementById("offcanvasDarkNavbar");
+const navBar = document.getElementById("navBar");
 
-  //images transition onload
+//images transition onload
 window.onload = () => {
-    imgEl.classList.add('show')
-    svgEl.classList.add('show')
-}
+  imgEl.classList.add("show");
+  svgEl.classList.add("show");
+};
 
-
-  //texts transition on scroll
+//texts transition on scroll
 const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if(entry.isIntersecting){
-            entry.target.classList.add('show')
-        }else{
-            entry.target.classList.remove('show')
-        }
-    })
-})
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
 
+hiddenEls.forEach((el) => observer.observe(el));
 
-hiddenEls.forEach((el) => observer.observe(el))
+// sections code
+// const observerSectios = new IntersectionObserver((entries) => {
+//   entries.forEach((entry) => {
+//     console.log(entry);
+//     if (entry.isIntersecting) {
+//       entry.target.classList.add("show");
+//     } else {
+//       entry.target.classList.remove("show");
+//     }
+//   });
+// });
 
-
-
+const sectionElemets = document.querySelectorAll(".text-figure");
+sectionElemets.forEach((el) => observer.observe(el));
